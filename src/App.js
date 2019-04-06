9b58df3c05e3d39793ac377b24271ec092480fa8
taqiabdulaziz <taqi_sting@hotmail.com>
2019-04-06 12:50:16 +0700

    MUI grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
+import { styles } from 'ansi-colors';
 
 const style = {
   container: {
-    maxWidth: "60%",
-    minWidth: "60%"
+    margin: "4%"
   },
   child: {
-    flex: 1
+    maxWidth: "70%",
+    minWidth: "70%",
+    margin: "40px"
+  },
+  a: {
+    color: "#c25400"
   }
 }
 
@@ -17,20 +22,37 @@ class App extends Component {
   render() {
     return (
       <Grid
-      className="body"
+        className="body"
         container
-        alignItems="center"
+        alignContent="center"
         justify="center"
+        style={style.container}
       >
         <Grid
-          direction="column"
-          alignItems="center"
-          justify="center"
-          style={style.container}
+          style={style.child}
         >
-          <h1 className="heading">Hi.</h1>
-          <h2 className="subheading">I'm Muhammad Taqi, a full stack developer at <a href="https://www.axiatadigital.com/">Axiata Digital Services Indonesia</a>, coffee consumer, reader, living in South Tangerang - Indonesia</h2>
-          <h1 className="heading">Hi. Projects</h1>
+          <Typography variant="h3" component="h3" gutterBottom>Hi.</Typography>
+          <Typography variant="h5" component="h5" gutterBottom>I'm Muhammad Taqi. a Full Stack Developer, coffee consumer, reader, currently working at Full Stack Developer at <a href="http://" style={style.a}>Axiata Digital Services Indonesia</a>, living in South Tangerang - Indonesia</Typography>
+        </Grid>
+
+        <Grid
+          style={style.child}
+        >
+          <Typography variant="h3" component="h3" gutterBottom>Projects</Typography>
+          <Grid
+            justify="flex-start"
+            alignItems="center"
+            container
+          >
+            <Grid style={{width: "20%"}} item xs={6}>
+              <Typography variant="h5" component="h5">test</Typography>
+              <Typography variant="h6" component="h5">Djoernal is an accounting mobile app which can be used by individuals as a personal revenue & expense tracker or used by micro-to-medium scale business for booking keeping with proper accounting procedures. This app allows the users to accurately see their incomes and expenses without possessing any prior accounting knowledge</Typography>
+            </Grid>
+            <Grid style={{width: "20%"}} item xs={6} direction="column" alignItems="flex-start" justify="flex-start">
+              <Typography variant="h5" component="h5">test</Typography>
+              <Typography variant="h6" component="h6">test</Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     );
